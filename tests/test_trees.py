3087205956c1b412bd2cd9aa3497b6aaabb4ec1a
liz-acosta <elizabeth.r.acosta@gmail.com>
2024-11-24 15:04:09 -0800

    good stopping point

--- tests/test_trees.py
+++ tests/test_trees.py
@@ -1,9 +1,12 @@
 import json
 import unittest
-from trees import extract_street_names
+import pandas as pd
+from trees import extract_street_names, preprocess_tree_data, preprocess_property_data, calculate_average_prices
 
 DUBLIN_TREES = "tests/test_data/test_dublin_trees.json"
 EXPECTED_DUBLIN_TREES = "tests/test_data/expected_test_dublin_trees.json"
+EXPECTED_AVERAGE_COST = "tests/test_data/expected_average_cost.json"
+TEST_DUBLIN_PROPERTY = "tests/test_data/test_dublin-property.csv"
 
 class TestTrees(unittest.TestCase):
  
@@ -20,5 +23,49 @@ class TestTrees(unittest.TestCase):
 
         self.assertEqual(len(test_results), len(expected_test_results["short"]))
         self.assertCountEqual(test_results, expected_test_results["short"])
+
+    def test_preprocess_tree_data(self):
         
+        test_tree_data = json.load(open(DUBLIN_TREES))
+        test_results = preprocess_tree_data(DUBLIN_TREES)
+
+        print("TEST RESULTS: ", test_results)
+
+        expected_test_results = json.load(open(EXPECTED_DUBLIN_TREES))
+
+        print("EXPECTED TEST RESULTS: ", expected_test_results)
+
+        self.assertCountEqual(test_results, expected_test_results)
+        self.assertEqual(list(test_results.keys()), ["short", "tall"])
+        self.assertEqual(test_results["short"], expected_test_results["short"])
+        self.assertEqual(test_results["tall"], expected_test_results["tall"])
+    
+    def test_preprocess_property_data(self):
         
+        test_dublin_property = pd.read_csv(TEST_DUBLIN_PROPERTY)
+        
+        test_results = preprocess_property_data(TEST_DUBLIN_PROPERTY)
+
+        print("TEST RESULTS: ", test_results)
+
+        expected_test_results = json.load(open(EXPECTED_AVERAGE_COST))
+
+        print("EXPECTED TEST RESULTS: ", expected_test_results)
+
+        self.assertTrue(pd.api.types.is_float_dtype(test_results["Price"]))
+     
+    def test_calculate_average_prices(self):
+        
+        test_preproccessed_data = json.load(open(EXPECTED_DUBLIN_TREES))
+        expected_test_results = json.load(open(EXPECTED_AVERAGE_COST))
+        test_dublin_property = preprocess_property_data(TEST_DUBLIN_PROPERTY)
+        
+        test_results = calculate_average_prices(test_dublin_property, test_preproccessed_data)
+
+        print("TEST RESULTS: ", test_results)
+
+        expected_test_results = json.load(open(EXPECTED_AVERAGE_COST))
+
+        print("EXPECTED TEST RESULTS: ", expected_test_results)
+
+        self.assertEqual(test_results, expected_test_results)             
--- trees.py
+++ trees.py
@@ -1,29 +1,88 @@
 import json
+import pandas as pd
 
 DUBLIN_TREES = "data/dublin_trees.json"
-
-# returns JSON object as a dictionary
-tree_data = json.load(open(DUBLIN_TREES))
+PROPERTY_DATA = "data/dublin-property.csv"
 
 def extract_street_names(tree_data):
     streets = []
 
     for key, value in tree_data.items():
-        print("STREETS: ", streets)
+        # print("STREETS: ", streets)
         
-        print("KEY: ", key)
-        print("VALUE: ", value)
+        # print("KEY: ", key)
+        # print("VALUE: ", value)
 
         if isinstance(value, dict):
-            print("IS INSTANCE")
+            # print("IS INSTANCE")
             streets.extend(extract_street_names(value))
         
         elif isinstance(value, (int, float)):
-            print("THE VALUE IS ", value)
-            print("THE KEY IS ", key)
+            # print("THE VALUE IS ", value)
+            # print("THE KEY IS ", key)
             if value > 0:
-                print("YES")
-                print("ADDING KEY: ", key)
+                # print("YES")
+                # print("ADDING KEY: ", key)
                 streets.append(key)
     
     return streets
+ 
+def preprocess_tree_data(data_filepath):
+
+    tree_data = json.load(open(data_filepath))
+
+    processed_data = {}
+    
+    for key, value in tree_data.items():
+        # print("KEY: ", key)
+        streets = extract_street_names(tree_data[key])
+        # print("STREETS: ", streets)
+        processed_data[key] = streets
+    
+    return processed_data
+
+def preprocess_property_data(data_filepath):
+    
+    property_data = pd.read_csv(data_filepath)
+
+    property_data["Price"] = property_data["Price"].str.replace('€', '', regex=True).replace(',', '', regex=True).astype(float)
+
+    print("PRICES: ", property_data["Price"])
+    
+    return property_data
+
+def calculate_average_prices(preprocessed_property_data, processed_tree_data):
+    
+    results = {}
+    
+    for category, streets in processed_tree_data.items():
+        print("CATEGORY: ", category)
+        print("STREETS: ", streets)
+        filtered = preprocessed_property_data[preprocessed_property_data["Street Name"].isin(streets)]
+        print("FILTERED: ", filtered)
+        print("NUMBER OF FILTERED: ", len(filtered))
+        
+        average_price = filtered["Price"].mean().round(2)
+
+        print("AVERAGE PRICE: ", average_price)
+        
+        results[category] = average_price
+    
+    return results
+
+def main():
+
+    print("Calcuating the average cost of properties according to tree height ... ")
+
+    preprocessed_tree_data = preprocess_tree_data(DUBLIN_TREES)
+
+    preprocessed_property_data = preprocess_property_data(PROPERTY_DATA)
+
+    calculated_averages = calculate_average_prices(preprocessed_property_data, preprocessed_tree_data)
+
+    print(f"Average price of properties on streets with tall trees: €{calculated_averages['tall']}")
+    print(f"Average price of properties on streets with short trees: €{calculated_averages['short']}")
+
+# Run the script
+if __name__ == "__main__":
+    main()
